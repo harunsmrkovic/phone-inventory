ba00a975a77ce8d6ab4546f24345c6e24307107b
Harun Smrkovic <harun@personal.com>
2016-03-25 19:30:05 +0100

    yoed angular

---
+++ angular/app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngCookies',
+    'ngResource',
+    'ngTouch'
+  ]);
